d2657b301e86879ae54c2aebfa7319ea7ea351ea
SAHIL000002 <asahilanshari2507@gmail.com>
2026-08-20 23:56:03 +0530

    rotate 1 element by right

--- array/rotate_array_by_1.js
+++ array/rotate_array_by_1.js
@@ -8,3 +8,13 @@ for (let i = 0; i < arr.length; i++) {
 arr[arr.length - 1] = firstElement;
 console.log(arr);
 
+// rotate 1 by right
+
+let arr2 = [1, 2, 3, 4, 5];
+let lastElement = arr2[arr2.length - 1];
+for (let i = arr2.length - 1; i > 0; i--) {
+  arr2[i] = arr2[i - 1];
+}
+arr2[0] = lastElement;
+
+console.log(arr2);
