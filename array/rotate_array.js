56319196793066106ed25274422ded6f8c1da5ff
SAHIL000002 <asahilanshari2507@gmail.com>
2026-08-22 01:23:08 +0530

    right rotation by k element

--- array/rotate_array.js
+++ array/rotate_array.js
@@ -12,3 +12,17 @@ for (let j = 0; j < k % arr.length; j++) {
 
 console.log(arr);
 
+// right rotation by k element
+
+arr2 = [1, 2, 3, 4, 5];
+let k2 = 2;
+for (let j = 0; j < k2 % arr2.length; j++) {
+  let copy = arr2[arr2.length - 1];
+  for (let i = arr2.length - 1; i > 0; i--) {
+    arr2[i] = arr2[i - 1];
+  }
+  arr2[0] = copy;
+}
+
+console.log(arr2);
+
