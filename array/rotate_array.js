691109d7fca7f9c21120ff1f01e846b014348f97
SAHIL000002 <asahilanshari2507@gmail.com>
2026-08-22 01:22:17 +0530

    left rotation by k element

---
+++ array/rotate_array.js
@@ -0,0 +1,14 @@
+// left rotation by k element
+
+arr = [1, 2, 3, 4, 5];
+let k = 10;
+for (let j = 0; j < k % arr.length; j++) {
+  let copy = arr[0];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[arr.length - 1] = copy;
+}
+
+console.log(arr);
+
