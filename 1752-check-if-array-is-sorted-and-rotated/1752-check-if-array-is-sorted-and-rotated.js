b62b1a8ee6bc4e02a0b31118fe945cc1be672091
SAHIL000002 <asahilanshari2507@gmail.com>
2026-08-23 23:05:54 +0530

    Time: 0 ms (100%), Space: 52.2 MB (97.15%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -8,15 +8,11 @@ var check = function (nums) {
         if (nums[i] > nums[i + 1]) {
             c++;
         }
-        if(nums[i+1] === undefined){
-            if(nums[nums.length-1]>nums[0]){
+        if (nums[i + 1] === undefined) {
+            if (nums[nums.length - 1] > nums[0]) {
                 c++;
             }
         }
     }
-    if (c < 2) {
-        return true;
-    } else {
-        return false;
-    }
+    return c < 2;
 };
